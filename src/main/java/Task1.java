56bcd9c9e6b481b599c293871fe65d42be58e50b
AlexandrNedilko <aleksandr_ned@ukr.net>
2019-06-03 10:22:01 +0300

    Initial commit

---
+++ src/main/java/Task1.java
@@ -0,0 +1,7 @@
+/*"1. Create a class ""Human"". A human should have the following attributes:
+        weight, age, first name, last name, height."
+        "1) Create a class Human with all the required fields to store the information (variable types you could choose by yourself).
+2) Add to the class a constructor with parameters which initialize all the fields."*/
+
+public class Task1 {
+}
