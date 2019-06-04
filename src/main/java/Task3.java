71edd5258b382c68ea00f2e0a6ecec0434a64e9a
AlexandrNedilko <aleksandr_ned@ukr.net>
2019-06-04 20:15:10 +0300

    Add task 2-8

---
+++ src/main/java/Task3.java
@@ -0,0 +1,7 @@
+/*"3. Create a class Dog.
+        The dog should have a name and an age (private).
+        Create getters and setter for all the fields."
+        1) When you will be creating setters - keep in mind that
+        fields shouldn't be empty and age can't be less of equals to zero.*/
+public class Task3 {
+}
